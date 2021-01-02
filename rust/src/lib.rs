f6f24c6e4059733ca0dfce261e08659e4730772e
Kostas Chalkias <kostascrypto@fb.com>
2021-01-01 20:51:47 -0800

    Add sparse Merkle tree support

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,3 +3,15 @@ mod hashes;
 mod hashwires;
 mod padding;
 mod shuffle;
+
+// For smt
+mod error;
+mod index;
+mod node_template;
+mod proof;
+mod traits;
+mod tree;
+mod utils;
+
+#[cfg(test)]
+mod tests;
